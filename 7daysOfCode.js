d25213f2be9f0847dd416bdcc9d74be5ea0d6eb0
ignaciomajo <ignacio.majoo@gmail.com>
2025-02-08 13:39:46 -0300

    UPDATE 7daysOfCode.js | Day 2

--- 7daysOfCode.js
+++ 7daysOfCode.js
@@ -43,31 +43,31 @@ let nombre = prompt("¿Cuál es tu nombre?")
 let edad = prompt("¿Cuántos años tienes?")
 let lenguajeProgramacion = prompt('¿Qué lenguaje de programación estás estudiando?')
 
-console.log(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguajeProgramacion}!`)
+alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguajeProgramacion}!`)
 
 let respuesta = prompt(`¿Te gusta estudiar ${lenguajeProgramacion}? Responde con el número 1 para SÍ o 2 para NO:`)
 
 if (respuesta == '' | isNaN(respuesta)) {
-    console.log('Por favor, ingresa una respuesta valida')
+    alert('Por favor, ingresa una respuesta valida')
     let respuesta = prompt(`¿Te gusta estudiar ${lenguajeProgramacion}? Responde con el número 1 para SÍ o 2 para NO:`)
 } else if (respuesta == 1) {
-    console.log('¡Muy bien! Sigue estudiando y tendrás mucho éxito.')
+    alert('¡Muy bien! Sigue estudiando y tendrás mucho éxito.')
 } else if (respuesta == 2) {
-    console.log('Oh, que pena... ¿Ya intentaste aprender otros lenguajes?')
+    alert('Oh, que pena... ¿Ya intentaste aprender otros lenguajes?')
     let preguntaLenguaje = prompt('Responde con 1 para SÍ o 2 para NO:')
     if (preguntaLenguaje == '' | isNaN(preguntaLenguaje)) {
-        console.log('Por favor, ingresa una respuesta válida')
+        alert('Por favor, ingresa una respuesta válida')
         let preguntaLenguaje = prompt('Responde con 1 para SÍ o 2 para NO:')
     } else if (preguntaLenguaje == 1) {
         let nuevoLenguaje = prompt('¿Que otro lenguaje has aprendido?: ')
         let lenguajeProgramacion = nuevoLenguaje
-        console.log(`¡Estupendo! Si te gusta más ${lenguajeProgramacion}, sigue estudiando y tendrás mucho éxito. 
-            Pero recuerda, a veces es útil conocer más de un lenguaje de programación aunque te especialices en alguno en particular.`)
+        alert(`¡Estupendo! Si te gusta más ${lenguajeProgramacion}, sigue estudiando y tendrás mucho éxito. 
+            Pero recuerda, puede resultar útil conocer más de un lenguaje de programación aunque te especialices en alguno en particular.`)
     } else if (preguntaLenguaje == 2) {
-        console.log('¡No te preocupes! Siempre puedes intentar aprender otro lenguaje de programación.')
+        alert('¡No te preocupes! Siempre puedes intentar aprender otro lenguaje de programación.')
     } else {
-        console.log('Por favor, ingresa una respuesta válida')
+        alert('Por favor, ingresa una respuesta válida')
     }
 } else {
-    console.log('Por favor, ingresa una respuesta válida')
+    alert('Por favor, ingresa una respuesta válida')
 }
