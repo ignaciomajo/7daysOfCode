e9f80a3a6453be2f718444616f9b43e8ab6a740f
ignaciomajo <ignacio.majoo@gmail.com>
2025-02-11 23:16:15 -0300

    VERSION FINAL app.js - index.html - style.css | Day 5

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 
+let listadoCategorias = []
 
+
+function limpiarCajasProductos() {
+    document.getElementById('prod_categoria').value = '';
+    document.getElementById('prod_producto').value = '';
+    document.getElementById('prod_cantidad').value = '';
+    return;
+}
+
+function limpiarCajaCategoria() {
+    document.getElementById('categoria').value = '';
+    return;
+}
 
 
 function crearCategoria() {
     let categoriaInput = document.getElementById('categoria');
-    let nombreCategoria = categoriaInput.value.trim();
+    let nombreCategoria = categoriaInput.value.trim().toUpperCase();
 
-    if (nombreCategoria !== '' && isNaN(Number(nombreCategoria))) {
+    if (listadoCategorias.includes(nombreCategoria)) {
+        alert('La categoría ya existe.');
+        limpiarCajaCategoria();
+    }
+     else if (nombreCategoria !== '' && isNaN(Number(nombreCategoria))) {
         
         let contenedorCategoria = document.createElement('div');
-        contenedorCategoria.classList.add(`categoria__${nombreCategoria}`);
+        contenedorCategoria.classList.add(`categoria__${nombreCategoria.toLowerCase()}`);
 
         let tituloCategoria = document.createElement('h3');
+        tituloCategoria.classList.add('notepad__listado__titulo')
         tituloCategoria.textContent = nombreCategoria;
 
         let lista = document.createElement('ul');
@@ -20,10 +38,62 @@ function crearCategoria() {
         contenedorCategoria.appendChild(tituloCategoria);
         contenedorCategoria.appendChild(lista);
         
-        document.querySelector('.notepad__listas').appendChild(contenedorCategoria);
+        document.querySelector('.notepad__listado__listas').appendChild(contenedorCategoria);
 
+        listadoCategorias.push(nombreCategoria);
         categoriaInput.value = ''
+
         } else {
             alert('Por favor, ingrese un nombre de categoría válido')
         }
+}
+
+function agregarProducto() {
+    let categoriaProducto = document.getElementById('prod_categoria').value.trim().toUpperCase();
+    let nombreProducto = document.getElementById('prod_producto').value.trim();
+    let cantidadProducto = document.getElementById('prod_cantidad').value.trim();
+
+    if (!listadoCategorias.includes(categoriaProducto)) {
+        alert('La categoría ingresada no existe, debes crearla o asignar el producto a otra categoría.');
+        limpiarCajasProductos();
+        return;
+    } else {
+
+        let categoriaElemento = document.getElementsByClassName(`categoria__${categoriaProducto.toLowerCase()}`);
+        let listaCategoria = categoriaElemento[0].querySelector('ul');
+        let li = document.createElement('li');
+        li.id = `${nombreProducto}`
+        li.textContent = `${nombreProducto}: ${cantidadProducto}`;
+        listaCategoria.appendChild(li);
+
+        limpiarCajasProductos();      
+    }
+    return;
+}
+
+
+function tacharProducto() {
+    let producto = document.getElementById('prod_producto').value.trim().toLowerCase();
+    if (!producto) {
+        alert('Debe ingresar un producto.');
+        limpiarCajasProductos();
+        return;
+    }
+
+    let lista = document.querySelectorAll('.notepad__listado__listas li');
+    let encontrado = false;
+
+    lista.forEach(item => {
+        let textoItem = item.textContent.trim().toLowerCase();
+        if (textoItem.includes(producto)) {  
+            item.style.textDecoration = 'line-through';
+            encontrado = true;
+        }
+    });
+
+    if (!encontrado) {
+        alert('El producto no pertenece a ninguna lista');
+    }
+
+    limpiarCajasProductos();
 }
